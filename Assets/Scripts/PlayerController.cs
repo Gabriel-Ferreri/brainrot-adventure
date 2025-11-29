2420b60db15905959508d6912f5d83a16f5b2822
JadanThayaseelan <99999689+BamShockDodge@users.noreply.github.com>
2025-11-29 14:37:43 +0000

    Added walking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,16 +3,38 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    bool isFacingRight = false;
+    float horizontal = 0.0f;
+
+    public float jumpForce = 7f;
+    public Transform groundCheck;
+    public float checkRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+    
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
+        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+        // Jump when pressing space
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);  // reset vertical speed
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
         if(Keyboard.current.leftArrowKey.isPressed)
         {
             horizontal = -1.0f;
@@ -21,11 +43,27 @@ public class PlayerController : MonoBehaviour
         {
             horizontal = 1.0f;
         }
+        else
+        {
+            horizontal = 0f;
+        }
+        
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 0.025f * horizontal;
         transform.position = position;
-        Debug.Log(horizontal);
         
+        flip();
+    }
+
+    void flip()
+    {
+        if(isFacingRight && horizontal < 0 || !isFacingRight && horizontal > 0)
+        {
+            Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
